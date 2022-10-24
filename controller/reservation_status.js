e5593669364b7d8ad8c245a13ac0b1d6de483d9d
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-10-24 09:32:11 +0700

    add send notif in reservation status

--- controller/reservation_status.js
+++ controller/reservation_status.js
@@ -4,6 +4,7 @@ const response = require('../utils/response')
 const reservationsService = require('../repository/reservations')
 const jwt = require('../middleware/jwt_auth')
 const { StatusCodes } = require('http-status-codes')
+const sendNotif = require('../utils/push_notification')
 
 reservationsStatus.get('/', jwt.validateToken, async (req, res) => {
     const { status, id } = req.query
@@ -11,9 +12,19 @@ reservationsStatus.get('/', jwt.validateToken, async (req, res) => {
         return response.responseFailure(res, StatusCodes.BAD_REQUEST, "Failure, need query arguments")
     }
     const data = await reservationsService.updateStatus(id, status)
-
     if (!data) {
         return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Failed update status")
+    }
+
+    const reservation = await reservationsService.getById(id)
+
+    let fcm = reservation.fcm_token
+
+    if (fcm) {
+        let isSuccess = await sendNotif.sendNotif(fcm, "Laporan Anda Diberikan", "body", "data")
+        if (!isSuccess) {
+            return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Sucess save in database but fail when send notif")
+        }
     }
 
     return response.responseSuccess(res, StatusCodes.OK, data, "Success update status")
