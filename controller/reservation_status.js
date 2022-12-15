d85c8d6885c2caa1b42faf033f6f1fc9ed691aec
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2022-12-15 16:27:53 +0700

    add notif

--- controller/reservation_status.js
+++ controller/reservation_status.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const reservationsStatus = express.Router()
 const response = require('../utils/response')
+const konsService = require('../repository/conselour')
+const pengawasService = require('../repository/pengawas')
 const reservationsService = require('../repository/reservations')
 const studentsService = require('../repository/students')
 const notifService = require('../repository/notifications_students')
+const notifPengawasService = require('../repository/notifications_conselour')
 const jwt = require('../middleware/jwt_auth')
 const { StatusCodes } = require('http-status-codes')
 const sendNotif = require('../utils/push_notification')
@@ -38,26 +41,45 @@ reservationsStatus.get('/', jwt.validateToken, async (req, res) => {
     const mahasiswa = await studentsService.getProfile(reservation.nim)
 
     let fcm = mahasiswa.fcm_token
-    let title, body
+    let title, body, title2, body2
     if (reservation.status == 4) {
         const setCounselor = await reservationsService.setKonselor(idConselour, id)
-        if (!setCounselor){
+        if (!setCounselor) {
             return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Failed set conselour")
         }
         title = "Permintaan Bimbingan Konseling Kamu Sedang Telah Dijadwalkan"
         body = "Konselor telah selesai memproses permintaan bimbingan konselingmu."
     } else if (reservation.status == 5) {
-        if (idConselour != reservation.id_conselour){
+        if (idConselour != reservation.id_conselour) {
             return response.responseFailure(res, StatusCodes.UNAUTHORIZED, "You are not allowed to use this endpoint")
         }
         title = "Permintaan Bimbingan Konseling Kamu Dalam Penanganan"
         body = "Mohon untuk menunggu sebentar, konselor akan menghubungi ke informasi kontak yang tersedia."
+    } else if (reservation.status == 6) {
+        title = "Bimbingan Konseling Telah Selesai"
+        body = `Bimbingan konseling pada tanggal ${reservation.reservation_time} telah selesai. Konselor sedang dalam proses menulis laporan akhir`
+
+        var konselor = await konsService.searchById(reservation.id_conselour)
+        title2 = `Laporan Akhir Sesi Bimbingan Konseling ${reservation.nim} Telah Selesai`
+        body2 = `Konselor ${konselor.name} telah selesai menulis laporan akhir sesi bimbingan konseling pada tanggal ${reservation.reservation_time}.`
+        var tokens = await pengawasService.getFcmToken()
+        if (tokens) {
+            let isSuccess = await sendNotif.sendNotifToAll(title2, body2, tokens)
+            if (!isSuccess) {
+                return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Sucess save in database but fail when send notif")
+            }
+        }
+        const saveNotif2 = await notifPengawasService.createNotif(title2, body2, reservation.id, 0, 0)
+        if (!saveNotif2) {
+            return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Fail save notif")
+        }
+
     }
     const saveNotif = await notifService.createNotif(reservation.nim, title, body, reservation.id, reservation.status)
     if (!saveNotif) {
         return response.responseFailure(res, StatusCodes.INTERNAL_SERVER_ERROR, "Fail save notif")
     }
-    if (fcm) {
+    if (mahasiswa.fcm_token) {
 
         let isSuccess = await sendNotif.sendNotif(fcm, title, body)
         if (!isSuccess) {
